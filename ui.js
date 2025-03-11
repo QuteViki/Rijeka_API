fa685b655244f4c66b4385c894d7ac65519210ef
QuteViki <edacalderini14@gmail.com>
2025-03-11 14:18:39 +0100

    2update
    added other needed html-s

---
+++ ui.js
@@ -0,0 +1,5 @@
+document.getElementById("gumb_eng").onclick = function () {
+    location.href = "/index_eng.html";}
+
+document.getElementById("gumb_hr").onclick = function () {
+    location.href = "/index.html";}
