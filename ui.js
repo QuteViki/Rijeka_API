251c96fa261317e123a88fd60f5bccc78a7f669c
QuteViki <edacalderini14@gmail.com>
2025-03-23 00:13:46 +0100

    update23032025
    Update of current code with some changes

--- ui.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("gumb_eng").onclick = function () {
-    location.href = "/index_eng.html";}
-
-document.getElementById("gumb_hr").onclick = function () {
-    location.href = "/index.html";}
